3fa1f6454cc671b8c39668a46bc691fc211c9693
Sonder 2x2 <animesnikanemejt@gmail.com>
2026-03-19 12:43:49 +0300

    добавил главную страницу

--- models.py
+++ models.py
@@ -57,3 +57,9 @@ class Product(db.Model):
 
     def __repr__(self):
         return f"<Product {self.name}>"
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
--- routes/main/main.py
+++ routes/main/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,render_template
 from flask_login import login_required, current_user, login_user, logout_user
+from models import Review
 
 main_bp = Blueprint('main', __name__)
 
@@ -7,4 +8,9 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/home')
 @main_bp.route('/')
 def home():
-    return render_template('main/index.html')
+    reviews = Review.query.order_by(Review.created_at.desc()).limit(3).all()
+    return render_template('main/index.html', reviews=reviews)
+
+@main_bp.route('/steam-topup')
+def steam_topup():
+    return render_template('main/steam_topup.html')
